817f49e7ca8b34568ef2f7651e5001f87d1f9513
Yang CHI <yangchi@cisco.com>
2014-08-19 23:06:55 -0700

    updated gitignore. added a dummy quick_find. updated test. test fails

--- dynamic_conn/dynamic_conn_test.go
+++ dynamic_conn/dynamic_conn_test.go
@@ -2,6 +2,23 @@ package dynamiccon
 
 import "testing"
 
-func Test(*testing.T) {
-	return
+func Test(t *testing.T) {
+	union(4, 3)
+	union(3, 8)
+	union(6, 5)
+	union(9, 4)
+	union(2, 1)
+	if connected(0, 7) {
+		t.Error("0 and 7 should not be connected")
+	}
+	if !connected(8, 9) {
+		t.Error("8 and 9 should be connected")
+	}
+	union(0, 5)
+	union(7, 2)
+	union(6, 1)
+	union(1, 0)
+	if !connected(0, 7) {
+		t.Error("0 and 7 should be connected")
+	}
 }
---
+++ dynamic_conn/quick_find.go
@@ -0,0 +1,9 @@
+package dynamiccon
+
+func union (first, second int)  {
+	return
+}
+
+func connected (first, second int) (bool) {
+	return false
+}
